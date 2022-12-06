469f0f5d2a6d4841d09e26327d92fca292e4918b
mizaCampos <mizaellauriano276@gmail.com>
2022-12-06 15:57:00 -0300

    perfil de teste, implementado

---
+++ src/main/java/com/bookstore/bookstoreapi/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.bookstore.bookstoreapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bookstore.bookstoreapi.service.DBService;
+
+
+@Profile("test")
+public class TestConfig {
+    
+    @Autowired
+    DBService dbService;
+
+    @Bean
+    public void instanciarBaseDeDados(){
+        this.dbService.instanciarBaseDeDados();
+    }
+}
+
+//url=jdbc:h2:mem:01bee9f9-2d08-475e-ba3a-41f424ca2dcd user=SA
+
+//jdbc:h2:mem:01bee9f9-2d08-475e-ba3a-41f424ca2dcd
--- src/main/java/com/bookstore/bookstoreapi/service/DBService.java
+++ src/main/java/com/bookstore/bookstoreapi/service/DBService.java
@@ -1,5 +1,39 @@
 package com.bookstore.bookstoreapi.service;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.bookstoreapi.domain.Categoria;
+import com.bookstore.bookstoreapi.domain.Livro;
+import com.bookstore.bookstoreapi.repositories.CategoriaRepository;
+import com.bookstore.bookstoreapi.repositories.LivroRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class DBService {
+
+    private LivroRepository livroRepository;
+	private CategoriaRepository categoriaRepository;
+
+    @Bean
+    public void instanciarBaseDeDados(){
+
+        Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Engenharia da Informatica", "Livros de TI");
+		Categoria cat3 = new Categoria(null, "Biologia", "Livros Quimica");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", cat1);
+		Livro l2 = new Livro(null, "Clean code", "Robert Martin", cat2);
+		Livro l3 = new Livro(null, "Clean code", "Robert Martin", cat3);
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3));
+    }
     
+
+
 }
